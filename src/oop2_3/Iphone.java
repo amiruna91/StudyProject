c3b1b48b3df145c21b75c6f86da9bd782d4ca588
jooyoung <amiruna91@gmail.com>
2020-03-23 22:44:17 +0900

    11일차 객체지향2(클래스 형변환)

---
+++ src/oop2_3/Iphone.java
@@ -0,0 +1,15 @@
+package oop2_3;
+
+public class Iphone extends SmartPhone{
+
+	String userid;
+	String password;
+	
+	void applemusic() {
+		System.out.println("애플뮤직을 듣습니다.");
+	}
+	
+	void siri() {
+		System.out.println("시리야~~~");
+	}
+}
---
+++ src/oop2_3/Phone.java
@@ -0,0 +1,14 @@
+package oop2_3;
+
+public class Phone {
+
+	String tel;
+	
+	void call() {
+		System.out.println("전화를 겁니다.");
+	}
+	
+	void receive() {
+		System.out.println("전화를 받습니다.");
+	}
+}
