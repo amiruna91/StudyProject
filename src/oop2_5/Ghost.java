3011d8cb5f097713be48d1ff2d95b8ca605dfc05
jooyoung <amiruna91@gmail.com>
2020-03-24 21:53:29 +0900

    12일차 객체지향2(추상클래스/메서드)

---
+++ src/oop2_5/Ghost.java
@@ -0,0 +1,10 @@
+package oop2_5;
+
+public class Ghost extends Unit{
+
+	@Override
+	void attack() {
+		System.out.println("[" + name + "]은 자신을 감추고 적을 공격합니다.");
+		
+	}
+}
---
+++ src/oop2_5/Overlord.java
@@ -0,0 +1,11 @@
+package oop2_5;
+
+public class Overlord extends Unit{
+	
+	// 수행문이 없어도 메서드 몸체만 있으면 구현 메서드다.
+	@Override
+	void attack() {
+	
+		
+	}
+}
