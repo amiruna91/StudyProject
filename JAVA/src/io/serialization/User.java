b3e9d8907429712733de5fef2e1ab2fbb78cff66
JooYoung <amiruna91@gmail.com>
2020-04-10 17:49:50 +0900

    2020-04-10 표준입출력&직렬화

---
+++ JAVA/src/io/serialization/User.java
@@ -0,0 +1,70 @@
+package serialization;
+
+import java.io.Serializable;
+
+public class User implements Serializable{
+ 
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private int no;
+	private String userId;
+	private String password;
+	private String tel;
+	private String email;
+	
+	// 직렬화 대상에서 제외된다.
+	private transient UserGrade grade;
+	
+	public User() {}
+
+	public int getNo() {
+		return no;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public UserGrade getGrade() {
+		return grade;
+	}
+	
+	public void setGrade(UserGrade grade) {
+		this.grade = grade;
+	}
+}
---
+++ JAVA/src/io/serialization/UserGrade.java
@@ -0,0 +1,27 @@
+package serialization;
+
+public class UserGrade {
+
+	private String grade;
+	private int point;
+	
+	public UserGrade() {}
+
+	public String getGrade() {
+		return grade;
+	}
+
+	public int getPoint() {
+		return point;
+	}
+
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+
+	public void setPoint(int point) {
+		this.point = point;
+	}
+	
+	
+}
