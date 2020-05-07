a6d9b09a01f16b94d696919c3bf9a48174c9a17b
JooYoung <amiruna91@gmail.com>
2020-05-07 11:46:53 +0900

    2020-05-07 문제 답안 수정

--- DataBase/Quiz/2020-05-06 다섯번째 퀴즈.sql
+++ DataBase/Quiz/2020-05-06 다섯번째 퀴즈.sql
@@ -13,7 +13,7 @@ order by 1;
 --3. 2007년에 입사한 사원의 아이디, 이름, 입사일을 조회하기
 select employee_id, first_name, hire_date
 from employees
-where to_char(hire_date, 'yyyy') = '2007'
+where hire_date >= '2007/01/01' and  hire_date < '2008/01/01'
 order by 1;
 
 --4. 급여가 5000이상 12000이하이고, 20번과 50번 부서에 소속된 사원의 아이디, 이름, 급여, 소속부서명을 
@@ -59,7 +59,7 @@ and A.salary >= b.min_salary * 2;
 select A.employee_id, A.first_name, A.hire_date, B.first_name, B.hire_date
 from employees A, employees B
 where a.manager_id = B.employee_id
-and A.hire_date <= B.hire_date
+and A.hire_date < B.hire_date
 order by 1;
 
 --9. Steven King과 같은 부서에서 근무하는 사원의 아이디와 이름을 조회하기
@@ -69,11 +69,16 @@ where A.first_name || ' ' || A.last_name = 'Steven King'
 and A.department_id = B.department_id
 order by 1;
 
+select employee_id, first_name
+from employees
+where department_id in (select department_id
+                        from employees
+                        where first_name = 'Steven' and last_name = 'King');
+
 --10. 관리자별 사원수를 조회하기, 관리자 아이디, 사원수를 출력한다. 관리자 아이디순으로 오름차순 정렬
 select manager_id, count(*)
 from employees
 group by manager_id
-having manager_id is not null
 order by 1;
 
 --11. 커미션을 받는 사원의 아이디, 이름, 급여, 커미션을 조회하기
@@ -98,7 +103,7 @@ select A.employee_id, A.first_name, A.hire_date
 from employees A, (select hire_date, department_id
                    from employees
                    where first_name = 'Ismael') B
-where A.hire_date = B.hire_date
+where to_char(A.hire_date, 'yyyy') = to_char(B.hire_date, 'yyyy')
 and A.department_id = B.department_id
 order by 1;
 
@@ -114,6 +119,27 @@ select B.gra, count(*)
 from employees A, job_grades B
 where A.salary >= b.lowest_sal and A.salary <= b.highest_sal
 group by B.gra
+order by 1;
+
+-- E도 포함해서 조회 해보자
+select gra, 0
+from job_grades
+where gra not in (select B.gra
+                  from employees A, job_grades B
+                  where A.salary >= b.lowest_sal and A.salary <= b.highest_sal
+                  group by B.gra)
+union
+select B.gra, count(*)
+from employees A, job_grades B
+where A.salary >= b.lowest_sal and A.salary <= b.highest_sal
+group by B.gra;
+
+select Y.gra, nvl(cnt,0)
+from (select B.gra, count(*) cnt
+      from employees A, job_grades B
+      where A.salary >= b.lowest_sal and A.salary <= b.highest_sal
+      group by B.gra) X, job_grades Y
+where X.gra(+) = Y.gra
 order by 1;
 
 --16. 입사자가 가장 적은 년도와 그 해에 입사한 사원수를 조회하기
@@ -202,6 +228,14 @@ where B.avg_salary <= 10000
 and A.department_id = B.department_id
 order by 1;
 
+-- 다른 방법
+select A.department_id, B.department_name, trunc(avg(A.salary)) dept_avg_salary
+from employees A, departments B
+where A.department_id = B.department_id
+group by A.department_id, B.department_name
+having avg(A.salary) <= 10000
+order by 1;
+
 --24. 사원들 중에서 자신 종사하고 있는 직종의 최대급여와 동일한 급여를 받는 사원의 아이디, 이름, 급여를
 --    조회하기
 select A.employee_id, A.first_name, A.salary
@@ -209,14 +243,23 @@ from employees A, jobs B
 where A.job_id = B.job_id
 and A.salary = b.max_salary;
 
-----25. 분석함수를 사용해서 사원들을 급여순으로 정렬하고, 순번을 부여했을 때 6~10번째 속하는 순번,
+----25. 분석함수를 사용해서 사원들을 급여순으로 내림차순 정렬하고, 순번을 부여했을 때 6~10번째 속하는 순번,
 --    사원의 아이디, 이름, 급여, 급여등급을 조회하기
-select row_number() over (order by salary desc) num, A.employee_id, A.first_name, A.salary, B.gra
-from employees A, job_grades B
-where A.salary >= b.lowest_sal and A.salary <= B.highest_sal;
+select row_number() over (order by salary desc) num
+     , employee_id, first_name, salary
+from employees;
 
-select num, employee_id, first_name, salary, gra
-from (select row_number() over (order by salary desc) num, A.employee_id, A.first_name, A.salary, B.gra
-      from employees A, job_grades B
-      where A.salary >= b.lowest_sal and A.salary <= B.highest_sal)
+select num, employee_id, first_name, salary
+from (select row_number() over (order by salary desc) num
+           , employee_id, first_name, salary
+      from employees)
 where num >= 6 and num <= 10;
+
+select A.num, A.employee_id, A.first_name, A.salary, B.gra
+from (select num, employee_id, first_name, salary
+      from (select row_number() over (order by salary desc) num
+                 , employee_id, first_name, salary
+           from employees)
+      where num >= 6 and num <= 10) A, job_grades B
+where A.salary >= B.lowest_sal and A.salary <= B.highest_sal
+order by 1;
