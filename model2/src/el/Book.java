c25f56a9759775b659162e2240d0e005902eba8c
jooyoung <amiruna91@gmail.com>
2020-06-29 16:54:56 +0900

    2020-06-29 jsp 모델2 연습 시작

---
+++ model2/src/el/Book.java
@@ -0,0 +1,67 @@
+package el;
+
+import java.util.Date;
+
+public class Book {
+
+	private int no;
+	private String title;
+	private String writer;
+	private int price;
+	private double discountRate;
+	private Date date;
+	private boolean soldOut;
+	private int stock;
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public double getDiscountRate() {
+		return discountRate;
+	}
+	public void setDiscountRate(double discountRate) {
+		this.discountRate = discountRate;
+	}
+	public int getDiscountPrice() {
+		return price - (int) (price * discountRate);
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public boolean isSoldOut() {
+		return soldOut;
+	}
+	public void setSoldOut(boolean soldOut) {
+		this.soldOut = soldOut;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	
+}
