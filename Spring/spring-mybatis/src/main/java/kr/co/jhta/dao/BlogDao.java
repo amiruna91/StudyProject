ed9722a048342b2fe81016be7475193bc84b9500
jooyoung <amiruna91@gmail.com>
2020-07-28 18:43:30 +0900

    2020-07-28 spring-mybatis 연결 연습

---
+++ Spring/spring-mybatis/src/main/java/kr/co/jhta/dao/BlogDao.java
@@ -0,0 +1,15 @@
+package kr.co.jhta.dao;
+
+import java.util.List;
+
+import kr.co.jhta.vo.Blog;
+
+public interface BlogDao {
+
+	void insertBlog(Blog blog);
+	Blog getBlogByNo(int blogNo);
+	List<Blog> getBlogs();
+	List<Blog> getBlogsByWriter(String writer);
+	void updateBlog(Blog blog);
+	void deleteBlog(int no);
+}
---
+++ Spring/spring-mybatis/src/main/java/kr/co/jhta/dao/UserDao.java
@@ -0,0 +1,25 @@
+package kr.co.jhta.dao;
+
+import java.util.List;
+
+import kr.co.jhta.vo.User;
+
+public interface UserDao {
+
+	/**
+	 * 지정된 아이디에 해당하는 사용자정보를 반환한다.
+	 * @param userId 조회할 사용자 아이디
+	 * @return 사용자정보가 포함된 User객체, null반환될 수 있음
+	 */
+	User getUserById(String userId);
+	
+	/**
+	 * 지정된 사용자 정보를 저장한다.
+	 * @param user
+	 */
+	void insertUser(User user);
+	
+	void deleteUserById(String userId);
+	List<User> getUsersByName(String name);
+	void updateUser(User user);
+}
